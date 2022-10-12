9a827dd774625370e065383d0a0379e2f0609cd9
Marvin Cely <mardancely@gmail.com>
2022-10-11 23:06:52 -0500

    feat: Add first excersice

---
+++ cely_md/funciones.js
@@ -0,0 +1,13 @@
+function cuadratica(a, b, esSuma) {
+  const dentroRaiz = Math.pow(b) - 4 * a * c;
+  const denominador = 2 * a;
+  let resultado;
+
+  if (esSuma === true) {
+    resultado = numerador + Math.SQRT2(dentroRaiz);
+  } else {
+    resultado = numerador - Math.SQRT2(dentroRaiz);
+  }
+
+  return resultado;
+}
