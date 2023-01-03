6b6afda6626cb7cb2f6c067c841a76afb02f2780
lsalinasp <117488134+lsalinasp@users.noreply.github.com>
2023-01-03 17:58:57 +0000

    Adding changes to the operators to work

--- LuSalinas/4-calculator-html-css-js/Display.js
+++ LuSalinas/4-calculator-html-css-js/Display.js
@@ -2,15 +2,15 @@ class Display {
     constructor(displayValorAnterior, displayValorActual) {
         this.displayValorActual = displayValorActual;
         this.displayValorAnterior = displayValorAnterior;
-        this.calculador = new Calculadora ();
+        this.calculador = new Calculadora();
         this.tipoOperacion = undefined;
         this.valorActual = '';
         this.valorAnterior = '';
         this.signos = {
-            sumar: "+",
-            dividir: "/",
-            multiplicar: "x",
-            restar: "-",
+            sumar: '+',
+            dividir: '/',
+            multiplicar: 'x',
+            restar: '-',
         }
     }
 
@@ -20,36 +20,38 @@ class Display {
     }
 
     borrarTodo() {
-        this.valorActual = "";
-        this.valorAnterior = "";
+        this.valorActual ='';
+        this.valorAnterior = '';
         this.tipoOperacion = undefined;
         this.imprimirValores();
     }
 
-    computar() {
-        this.tipoOperacion !== "igual" && this.calcular();
+    computar(tipo) {
+        this.tipoOperacion !== 'igual' && this.calcular();
         this.tipoOperacion = tipo;
         this.valorAnterior = this.valorActual || this.valorAnterior;
-        this.valorActual = "";
+        this.valorActual = '';
         this.imprimirValores();
     }
 
     agregarNumero(numero) {
-        if(numero === "." && this.valorActual.includes(".")) return
+        if(numero === '.' && this.valorActual.includes('.')) return
         this.valorActual = this.valorActual.toString() + numero.toString();
         this.imprimirValores();
     }   
 
     imprimirValores() {
         this.displayValorActual.textContent = this.valorActual;
-        this.displayValorAnterior.textContent = this.valorAnterior;
+        this.displayValorAnterior.textContent = this.valorAnterior
     }
 
     calcular() {
         const valorAnterior = parseFloat(this.valorAnterior);
         const valorActual = parseFloat(this.valorActual);
 
-        if( isNaN(valorActual) || isNaN(valorAnterior)) return
+        //preguntar si son numeros y si cualquiera de las dos es Nan no operar nada
+        // en el caso de que si sean valores se debe update el valor actual
+        if(isNaN(valorActual) || isNaN(valorAnterior)) return
         this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
     }
 }
--- LuSalinas/4-calculator-html-css-js/index.js
+++ LuSalinas/4-calculator-html-css-js/index.js
@@ -6,12 +6,9 @@ const botonesOperadores = document.querySelectorAll (".operador");
 const display = new Display(displayValorAnterior, displayValorActual);
 
 botonesNumeros.forEach(boton => {
-    boton.addEventListener("click", () => {
-        display.agregarNumero(boton.innerHTML);
-
+    boton.addEventListener("click", () => display.agregarNumero(boton.innerHTML));
     }); 
-})
 
 botonesOperadores.forEach(boton => {
     boton.addEventListener("click", () => display.computar(boton.value))
-})
+});
