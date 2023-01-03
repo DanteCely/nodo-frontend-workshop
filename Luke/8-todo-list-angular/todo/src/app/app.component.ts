2747dd3bc6e4ace9aa00d5b4ac956488d26647b5
Zokam-LS <lucassy07@outlook.com>
2023-01-02 22:45:15 -0500

    Start of To Do List

---
+++ Luke/8-todo-list-angular/todo/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items(){
+    if(this.filter === 'all'){
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: string){
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+}
