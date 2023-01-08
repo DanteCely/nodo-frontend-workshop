d287f5660669682919279f619014a4c5082134b8
jgrisal6 <grisalesjuanpablo18@gmail.com>
2023-01-08 01:46:15 -0500

    Estructura y parte visual calculadora. Faltando la funcionalidad

--- grisales_jp/4-calculator-html-css-js/operaciones.js
+++ grisales_jp/4-calculator-html-css-js/operaciones.js
@@ -1,15 +1,34 @@
 import botones from './botones.json' assert {type:'json'};
 
-const Calculadora = document.getElementById('calculadora');
+const Calculadora = document.querySelector('#calculadora');
+const result = document.getElementById('#pantalla');
 
 botones.forEach( (value) => {
-    const Button = document.createElement('button', {type:'button'});
-    Button.innerText = value.content;
-    Button.style.backgroundColor = value.color;
+    const { content, type, className } = value;
+
+    const Button = document.createElement('button');
+    Button.innerText = content;
+    Button.classList.add(type);
+
+    if(className) {
+        Button.classList.add(className);
+    }
 
     Button.onclick = () => {
         console.log({ content: value.content });
-    }
 
+        /*/Funcionamiento números o puntos
+        if(type === "number"){
+
+        }
+
+        //Operando
+        else if(type === "operator") {
+
+        }
+        else(type === "set") {
+            ;
+        }*/
+    }
     Calculadora.appendChild(Button);
 })
