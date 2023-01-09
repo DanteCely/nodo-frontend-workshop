ae77cca9eaf992b17ec4ddfb53785fd54bae894b
jgrisal6 <grisalesjuanpablo18@gmail.com>
2023-01-09 12:19:24 -0500

    Actualización con funciones para proceso de calculadora. Falta arreglar el proceso de igualdad para que me arroje el resultado final

--- grisales_jp/4-calculator-html-css-js/operaciones.js
+++ grisales_jp/4-calculator-html-css-js/operaciones.js
@@ -1,7 +1,11 @@
 import botones from './botones.json' assert {type:'json'};
 
 const Calculadora = document.querySelector('#calculadora');
-const result = document.getElementById('#pantalla');
+let numero = "";
+
+const resultado = document.querySelector('#pantalla');
+resultado.innerHTML = "0";
+Calculadora.appendChild(resultado)
 
 botones.forEach( (value) => {
     const { content, type, className } = value;
@@ -14,21 +18,37 @@ botones.forEach( (value) => {
         Button.classList.add(className);
     }
 
+    console.log({className});
+
     Button.onclick = () => {
         console.log({ content: value.content });
 
-        /*/Funcionamiento números o puntos
-        if(type === "number"){
-
+        //Funcionamiento números o puntos
+        if(type === 'number'){
+            numero += content;
+            resultado.innerHTML = numero;
         }
 
         //Operando
-        else if(type === "operator") {
-
+        else if(type === 'operator' || type === 'equal') {
+            if(content == "=") {
+                let igual = eval(numero);
+                resultado.innerHTML = igual;
+                numero = igual;
+            }
+            else {
+                numero += content;
+            }
         }
-        else(type === "set") {
-            ;
-        }*/
+        else {
+            if (content == "C") {
+                numero = "";
+            }
+            else {
+                numero = numero.substring(0, numero.length-1);
+            }
+            resultado.innerHTML = numero;
+        }
     }
     Calculadora.appendChild(Button);
-})
+});
